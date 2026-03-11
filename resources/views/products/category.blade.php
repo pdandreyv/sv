261cf3d2c6abe085f847ca8affe3f5ca701dfcad
Andrey Poddubniy <pdandreyv@gmail.com>
2026-03-11 21:26:27 +0200

    страница подкатегорий

--- resources/views/products/category.blade.php
+++ resources/views/products/category.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="title">
+<section class="goods-catalog-page category-catalog-page">
+    <div class="title goods-page-title">
         <h2>{{$currentCategory->title}}</h2>
     </div>
 
-    {{-- Хлебные крошки --}}
     @if(!empty($breadcrumb) && count($breadcrumb))
-        <nav aria-label="breadcrumb" class="mb-3">
+        <nav aria-label="breadcrumb" class="mb-3 category-breadcrumbs">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="{{ route('products.goods') }}">Категории</a></li>
                 @foreach($breadcrumb as $idx => $bc)
@@ -24,27 +24,22 @@
     @endif
 
     @if ($currentCategory->description)
-        <div class="col-12 row description">
-            <br><br>
-            <div class="alert alert-info col-12">
-                {!! $currentCategory->description !!}
-            </div>
+        <div class="alert alert-info category-description">
+            {!! $currentCategory->description !!}
         </div>
     @endif
 
-    {{-- Если есть подкатегории, рисуем их как на главной странице категорий и НЕ показываем список товаров --}}
     @if(isset($childCategories) && $childCategories->count() > 0)
-        <div class="panel-categories">
-            <br>
-            <div class="row">
+        <div class="panel-categories goods-catalog-grid-wrap">
+            <div class="row goods-catalog-grid">
                 @foreach ($childCategories as $category)
-                    <div class="col-md-2 category-item">
+                    <div class="col-12 category-item goods-card-col">
                         <a href="{{ route('products.category', ['category_id' => $category->id]) }}" title="{{$category->description}}">
                             <div class="photo">
                                 @if ($category->photo && file_exists(public_path().'/images/product_category_photos/'.$category->photo))
                                     <img class="cat-img" src="/images/product_category_photos/{{$category->photo}}" alt="{{$category->title}}">
                                 @else
-                                    <img src="/images/placeholder.png" alt="temporary_no_photo">
+                                    <img class="cat-img" src="/images/placeholder.png" alt="temporary_no_photo">
                                 @endif
                             </div>
                             <div class="title">
@@ -54,78 +49,28 @@
                     </div>
                 @endforeach
             </div>
-            <br><br>
         </div>
     @else
-
-        <table class="table table-bordered table-categories">
-          <tr>
-            <td class="col-md-2"><div class="title-item link">
-                        <h4>Название</h4>
-                </div></td>
-            <td class="col-md-8"><div class="title-item">
-                        <h4>Описание</h4>
-                    </div></td>
-            <td class="col-md-2"><div class="title-item author">
-                        <h4>Автор</h4>
-                    </div></td>
-               
-          </tr>
-           @foreach ($products as $product)
-            <?php $images = $product->image()->where('main', 1)->first(); ?>
-          <tr>
-            <td class="col-md-2">
-                <a href="{{ route('products.product', ['product_id' => $product->id]) }}" title="{{$product->title}}">
-                    <div class="photo">
-                        @if ($images && file_exists(public_path().'/images/products/'.$images->new_name))
-                            <img src="/images/products/{{$images->new_name}}" alt="{{$product->title}}">
-                        @else
-                            <img src="/images/placeholder.png" alt="temporary_no_photo">
-                        @endif
-                    </div>
-                 </a>
-            </td>
-            <td class="col-md-8">
-               <a href="{{ route('products.product', ['product_id' => $product->id]) }}" title="{{$product->title}}" class="col-12">
-                        <div class="title">
-                            <h4>{{$product->title}}</h4>
+        <div class="row category-products-grid">
+            @foreach ($products as $product)
+                @php $images = $product->image()->where('main', 1)->first(); @endphp
+                <div class="col-lg-4 col-md-6 col-12 product-card-col">
+                    <a href="{{ route('products.product', ['product_id' => $product->id]) }}" class="product-card-link" title="{{$product->title}}">
+                        <div class="product-card">
+                            <div class="product-card-photo">
+                                @if ($images && file_exists(public_path().'/images/products/'.$images->new_name))
+                                    <img src="/images/products/{{$images->new_name}}" alt="{{$product->title}}">
+                                @else
+                                    <img src="/images/placeholder.png" alt="temporary_no_photo">
+                                @endif
+                            </div>
+                            <h4 class="product-card-title">{{$product->title}}</h4>
+                            <div class="product-card-price">{{$product->price}} грн</div>
                         </div>
                     </a>
-                    <div class="col-12 description">
-                        {{$product->description}}
-                    </div>
-                    <div class="col-12 production_place">
-                        <p><strong>Место производства:</strong> {{$product->production_place}}</p>
-                    </div>
-                    <div class="col-12 price">
-                        <p><strong>Цена:</strong> {{$product->price}} грн</p>
-                    </div>
-                    @if(!$product->is_service)
-                    <div class="col-12 add_cart">
-                        <input type="number" class="form-control quantity" name="" value="1"/>
-                        <input type="button" class="add_cart_btn btn" class="btn" value="В корзину"/>
-                        <input type="hidden" class="product_id" value="{{$product->id}}"/>
-                    </div>
-                    @endif
-            </td>
-            <td class="col-md-2">
-               <a href="{{ route('profile.show', ['id' => $product->user_id]) }}">
-                        <div class="title">
-                            <h4>{{$product->last_name.' '.str_limit($product->first_name, 1,'.').str_limit($product->middle_name, 1, '.')}}</h4>
-                        </div>
-                        <div class="photo">
-                            @if ($product->user_photo)
-                                <img src="/images/users_photos/{{$product->user_photo}}" alt="{{$product->title}}">
-                            @else
-                                <img src="/images/placeholder.png" alt="temporary_no_photo">
-                            @endif
-                        </div>
-                    </a>
-            </td>     
-          </tr>
-          @endforeach
-        </table>
+                </div>
+            @endforeach
+        </div>
     @endif
-    </div>
-    
+</section>
 @endsection
